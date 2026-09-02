9ee1f3fcfaefb133100410d5cc14f46109296783
Caio1234-svg <caioyukioyazawa@gmail.com>
2026-09-02 12:49:34 -0300

    atualizando o projeto lab 4

--- core/src/main/java/br/mackenzie/Main.java
+++ core/src/main/java/br/mackenzie/Main.java
@@ -5,13 +5,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.awt.*;
+import java.util.Iterator;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import java.util.Iterator;import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import java.util.Iterator;import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import java.util.Iterator;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
     private Texture backgroundImage;
+    private Texture obstacleTexture;
+    private Array<Rectangle> obstacles;
+    private float obstacleTimer;
 
 
     private float x, y;
@@ -25,15 +40,36 @@ public class Main extends ApplicationAdapter {
         batch = new SpriteBatch();
         image = new Texture("images.jpeg");
         backgroundImage = new Texture("background.png");
+        obstacleTexture = new Texture("drop.png");
+        obstacles = new Array<>();
 
         shipWidth = image.getWidth();
         shipHeight = image.getHeight();
 
 
+
+
+        x = Gdx.graphics.getWidth() / 2f - shipWidth / 2f;
+        y = Gdx.graphics.getHeight() / 2f - shipHeight / 2f;
+    }
+    public void create() {
+        ...
         x = Gdx.graphics.getWidth() / 2f - shipWidth / 2f;
         y = Gdx.graphics.getHeight() / 2f - shipHeight / 2f;
     }
 
+    private void createObstacle() {
+        float obstacleWidth = 50;
+        float obstacleHeight = 50;
+        float x = MathUtils.random(0, Gdx.graphics.getWidth() - obstacleWidth);
+        Rectangle obstacle = new Rectangle(x, Gdx.graphics.getHeight(), obstacleWidth, obstacleHeight);
+        obstacles.add(obstacle);
+    }
+
+    @Override
+    public void render() {
+    ...
+    }
     @Override
     public void render() {
         float delta = Gdx.graphics.getDeltaTime(); // tempo desde o último frame
@@ -59,12 +95,34 @@ public class Main extends ApplicationAdapter {
 
         x = Math.max(0, Math.min(x, Gdx.graphics.getWidth() - shipWidth));
         y = Math.max(0, Math.min(y, Gdx.graphics.getHeight() - shipHeight));
+        obstacleTimer += delta;
+        if (obstacleTimer > 1f) {
+            obstacleTimer = 0;
+            createObstacle();
+        }
+
+        Rectangle shipRect = new Rectangle(x, y, shipWidth, shipHeight);
+
+        Iterator<Rectangle> iter = obstacles.iterator();
+        while (iter.hasNext()) {
+            Rectangle obstacle = iter.next();
+            obstacle.y -= 200 * delta;
+
+            if (obstacle.y + obstacle.height < 0) {
+                iter.remove();
+            } else if (obstacle.overlaps(shipRect)) {
+                iter.remove();
+            }
+        }
 
 
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
         batch.draw(image, x, y);
         batch.end();
+        for (Rectangle obstacle : obstacles) {
+            batch.draw(obstacleTexture, obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+        }
     }
 
     @Override
